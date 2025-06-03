8d060645b59846a151558165f61f7e590abe11f0
Dylyk Oidopov <9dylyk@gmail.com>
2025-06-03 15:19:29 +0900

    fix: update base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Это важно для GitHub Pages
+  base: '/mvp-dental-webapp/', // Путь к репозиторию на GitHub Pages
 }) 
