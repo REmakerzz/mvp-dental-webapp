e3c588a3b8865c0b6f3f95a2dafb1b9229e85b89
Dylyk Oidopov <9dylyk@gmail.com>
2025-06-03 15:56:08 +0900

    deploy: add automatic path fixing and update warning

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'fix-asset-paths',
+      transformIndexHtml(html) {
+        // Автоматически исправляем абсолютные пути на относительные
+        return html.replace(/href="\/assets\//g, 'href="assets/')
+                  .replace(/src="\/assets\//g, 'src="assets/');
+      },
+    },
+  ],
   base: '/mvp-dental-webapp/', // Путь к репозиторию на GitHub Pages
   build: {
     // Гарантируем, что все ассеты будут использовать относительные пути
