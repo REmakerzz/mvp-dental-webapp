4ea523ebc27d2bc7fe21f454835af93923fe38a1
Dylyk Oidopov <9dylyk@gmail.com>
2025-06-03 16:13:28 +0900

    deploy: clean config, only correct base for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'fix-asset-paths',
-      transformIndexHtml(html) {
-        // Заменяем абсолютные пути на относительные
-        return html.replace(/href="\/assets\//g, 'href="assets/')
-                  .replace(/src="\/assets\//g, 'src="assets/');
-      },
-    },
-  ],
+  plugins: [react()],
   base: '/mvp-dental-webapp/', // Базовый путь для GitHub Pages
   build: {
     assetsDir: 'assets',
